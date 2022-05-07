17f0105e8be9956357417e29c3a4e253e098aa58
Elia65 <99482094+Elia65@users.noreply.github.com>
2022-05-07 18:04:17 +0300

    Add files via upload

---
+++ SAYNA-JAVASCRIPT-NOWAYHOME-042022/Assets/js/index.js
@@ -0,0 +1,8 @@
+document.querySelector('.inscription').addEventListener('click', function(e) {
+    var ok = "inscription effectuée !!! "
+    alert(ok)
+});
+
+var rs = document.getElementsByClassName('media').querySelector(Image);
+
+rs.style.hover.color = "white";
